9b7cbdcdcfef7501be76014a186830265ef875f4
Huixin Tan <huixintan91@gmail.com>
2021-11-13 14:51:08 +0800

    completed a working task manager without CSS

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,51 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    # @tasks = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new(task_params)
+    @tasks.save
+
+    redirect_to task_path(@tasks)
+  end
+
+  def edit
+    # @tasks = Task.find(params[:id])
+  end
+
+  def update
+    # @tasks = Task.find(params[:id])
+    @tasks.update(task_params)
+
+    redirect_to task_path(@tasks)
+  end
+
+  def destroy
+    # @tasks = Task.find(params[:id])
+    @tasks.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @tasks = Task.find(params[:id])
+  end
 end
